8fb389320f93528f28177d59a578d43d344da1df
Vaidic Joshi <vaidic@vaidicjoshi.in>
2021-09-18 03:44:19 +0530

    Created Test placeholder

--- src/main/java/in/vaidicjoshi/geektrust/backend/family/service/RelationshipService.java
+++ src/main/java/in/vaidicjoshi/geektrust/backend/family/service/RelationshipService.java
@@ -4,11 +4,4 @@ package in.vaidicjoshi.geektrust.backend.family.service;
  * @author Vaidic Joshi
  * @date 18/09/21
  */
-public class RelationshipService {
-
-
-
-
-
-}
-
+public class RelationshipService {}
